5a08c90fa7323db8cd108f5afe4fe78ae38a3447
N van Baren <nicolette_van_baren@gmail.com>
2014-11-05 09:54:10 +0100

    Remove installation files from application.
    Remove installation files from application.

--- Application/f2000/application/shared_components/navigation/tabs/standard/t_tab_menu_list.sql
+++ Application/f2000/application/shared_components/navigation/tabs/standard/t_tab_menu_list.sql
@@ -1,6 +1,6 @@
 --application/shared_components/navigation/tabs/standard/t_tab_list
 wwv_flow_api.create_tab (
-  p_id=> 5836132638455388 + wwv_flow_api.g_id_offset,
+  p_id=> 5836122638455388 + wwv_flow_api.g_id_offset,
   p_flow_id=> wwv_flow.g_flow_id,
   p_tab_set=> 'T_REGION',
   p_tab_sequence=> 25,
